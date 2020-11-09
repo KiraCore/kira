5e639a168ffd50694ca39429304ba41a4110fe68
James Truong <trueeyedev@gmail.com>
2020-11-10 02:11:03 +1100

    [KIP_51] update validator dockerfile

--- docker/base-image/container/deployment.sh
+++ docker/base-image/container/deployment.sh
@@ -21,72 +21,13 @@ add-apt-repository "deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ bionic un
 echo "APT Update, Upfrade and Intall..."
 apt-get update -y --fix-missing
 apt-get install -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-    autoconf \
-    automake \
-    apt-utils \
-    awscli \
-    dconf-editor \
     build-essential \
-    bind9-host \
-    bzip2 \
-    coreutils \
-    clang \
-    cmake \
-    dnsutils \
-    dpkg-dev \
-    ed \
-    file \
-    gcc \
-    g++ \
     git \
-    gnupg2 \
-    groff \
-    htop \
     hashdeep \
-    imagemagick \
-    iputils-tracepath \
-    iputils-ping \
-    jq \
-    language-pack-en \
-    libtool \
-    libzip4 \
-    libssl1.1 \
-    libudev-dev \
-    libunwind-dev \
-    libusb-1.0-0-dev \
-    locales \
     make \
-    nano \
-    nautilus-admin \
-    nginx \
-    netbase \
-    netcat-openbsd \
-    net-tools \
     nodejs \
     node-gyp \
-    openssh-client \
-    openssh-server \
-    pkg-config \
-    python \
-    patch \
-    procps \
-    python3 \
-    python3-pip \
-    rename \
-    rsync \
-    socat \
-    sshfs \
-    stunnel \
-    subversion \
-    syslinux \
     tar \
-    telnet \
-    tzdata \
     unzip \
-    wipe \
-    xdotool \
     yarn \
     zip
-
-# https://linuxhint.com/install_aws_cli_ubuntu/
-aws --version
