31a9884280da58959b7d4fa0fe8ae06434ae1b61
James Truong <trueeyedev@gmail.com>
2020-11-19 03:22:35 +1100

    [initial-validator] kms image name

--- workstation/scripts/update-base-image.sh
+++ workstation/scripts/update-base-image.sh
@@ -10,7 +10,7 @@ BASE_IMAGE_EXISTS=$($WORKSTATION_SCRIPTS/image-updated.sh "$KIRA_DOCKER/base-ima
 if [ "$BASE_IMAGE_EXISTS" == "False" ]; then
     # todo: delete valdiator, sentry, kms, frontend images
     # $WORKSTATION_SCRIPTS/delete-image.sh "$KIRA_DOCKER/frontend-image" "frontend-image"
-    $WORKSTATION_SCRIPTS/delete-image.sh "$KIRA_DOCKER/kms-image" "kms-image"
+    $WORKSTATION_SCRIPTS/delete-image.sh "$KIRA_DOCKER/kms" "kms"
     $WORKSTATION_SCRIPTS/delete-image.sh "$KIRA_DOCKER/sentry" "sentry"
     $WORKSTATION_SCRIPTS/delete-image.sh "$KIRA_DOCKER/validator" "validator"
 
--- workstation/scripts/update-kms-image.sh
+++ workstation/scripts/update-kms-image.sh
@@ -14,7 +14,7 @@ if [ "$KMS_IMAGE_EXISTS" == "False" ]; then
     echo "All imags were updated, starting kms image..."
     $WORKSTATION_SCRIPTS/update-image.sh "$KIRA_DOCKER/kms" "kms" "latest" "$SEKAI_INTEGRITY" "REPO=$SEKAI_REPO" "BRANCH=$SEKAI_BRANCH" #4
 elif [ "$KMS_IMAGE_EXISTS" == "True" ]; then
-    echo "INFO: kms-image is up to date"
+    echo "INFO: kms image is up to date"
 else
     echo "ERROR: Failed to test if kms image exists"
     exit 1
