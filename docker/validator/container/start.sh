3097d3396710ed3242772d39934db0717fa8fff9
James Truong <trueeyedev@gmail.com>
2020-11-24 14:36:39 +1100

    [KIP_51] mnemonic generate

--- docker/validator/container/start.sh
+++ docker/validator/container/start.sh
@@ -18,10 +18,10 @@ sekaid add-genesis-account $(sekaid keys show test -a --keyring-backend=test --h
 sekaid keys add frontend --keyring-backend=test --home=$SEKAID_HOME
 sekaid add-genesis-account $(sekaid keys show frontend -a --keyring-backend=test --home=$SEKAID_HOME) 1000000000ukex,1000000000validatortoken,1000000000stake --home=$SEKAID_HOME
 
-yes "chief drip promote message thing bread describe morning tiger box toe supreme quit during occur nest abstract brown paddle resource antique decide draw matter" | sekaid keys add signer --keyring-backend=test --home=$SEKAID_HOME --recover
+yes $SIGNER_MNEMONIC | sekaid keys add signer --keyring-backend=test --home=$SEKAID_HOME --recover
 sekaid add-genesis-account $(sekaid keys show signer -a --keyring-backend=test --home=$SEKAID_HOME) 1000000000ukex,1000000000validatortoken,1000000000stake --home=$SEKAID_HOME
 
-yes "equip exercise shoot mad inside floor wheel loan visual stereo build frozen potato always bulb naive subway foster marine erosion shuffle flee action there" | sekaid keys add faucet --keyring-backend=test --home=$SEKAID_HOME --recover
+yes $FAUCET_MNEMONIC | sekaid keys add faucet --keyring-backend=test --home=$SEKAID_HOME --recover
 sekaid add-genesis-account $(sekaid keys show faucet -a --keyring-backend=test --home=$SEKAID_HOME) 1000000000ukex,1000000000validatortoken,1000000000stake --home=$SEKAID_HOME
 
 sekaid gentx-claim validator --keyring-backend=test --moniker="hello" --home=$SEKAID_HOME
