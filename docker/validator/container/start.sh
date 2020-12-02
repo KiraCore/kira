797c20defab68c0d18182ed45a0538c4ef4a6c6c
James Truong <trueeyedev@gmail.com>
2020-12-02 21:39:33 +1100

    [KIP_51] validator grpc disable

--- docker/validator/container/start.sh
+++ docker/validator/container/start.sh
@@ -35,4 +35,4 @@ sekaid add-genesis-account $(sekaid keys show faucet -a --keyring-backend=test -
 
 sekaid gentx-claim validator --keyring-backend=test --moniker="hello" --home=$SEKAID_HOME
 
-sekaid start --home=$SEKAID_HOME
+sekaid start --home=$SEKAID_HOME --grpc.enable=false
