ab87199a9274dab421ad0e3d70ce025a6ad0d67a
James Truong <trueeyedev@gmail.com>
2020-12-03 01:22:50 +1100

    [KIP_51] remove 9090 config for sentry

--- docker/sentry/container/start.sh
+++ docker/sentry/container/start.sh
@@ -10,7 +10,7 @@ rm -rf $SEKAID_HOME/config
 
 sekaid init --chain-id=testing testing --home=$SEKAID_HOME
 cp $SELF_CONFIGS/genesis.json $SEKAID_HOME/config/genesis.json
-# cp $SELF_CONFIGS/config.toml $SEKAID_HOME/config/config.toml
+cp $SELF_CONFIGS/config.toml $SEKAID_HOME/config/config.toml
 cp $SELF_CONFIGS/node_key.json $SEKAID_HOME/config/node_key.json
 
 sekaid keys add test --keyring-backend=test --home=$SEKAID_HOME
