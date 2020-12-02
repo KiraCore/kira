ffcf3a7c5615e81931f6b261f3f9f640212f3156
James Truong <trueeyedev@gmail.com>
2020-12-03 02:13:19 +1100

    [KIP_51] sentry genesis from validator

--- docker/sentry/container/start.sh
+++ docker/sentry/container/start.sh
@@ -13,9 +13,4 @@ cp $SELF_CONFIGS/genesis.json $SEKAID_HOME/config/genesis.json
 cp $SELF_CONFIGS/config.toml $SEKAID_HOME/config/config.toml
 cp $SELF_CONFIGS/node_key.json $SEKAID_HOME/config/node_key.json
 
-sekaid keys add test --keyring-backend=test --home=$SEKAID_HOME
-sekaid add-genesis-account $(sekaid keys show test -a --keyring-backend=test --home=$SEKAID_HOME) 1000000000ukex,1000000000validatortoken,1000000000stake --home=$SEKAID_HOME
-
-sekaid gentx-claim test --keyring-backend=test --moniker="hello" --home=$SEKAID_HOME
-
 sekaid start --home=$SEKAID_HOME --rpc.laddr="tcp://0.0.0.0:26657"
