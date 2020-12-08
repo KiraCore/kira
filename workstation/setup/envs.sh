8814bbd193471e2f1fe09dd1718c63b1fd5ca14f
James Truong <trueeyedev@gmail.com>
2020-12-09 09:22:44 +1100

    [KIP_51] env updated

--- workstation/setup/envs.sh
+++ workstation/setup/envs.sh
@@ -54,7 +54,7 @@ mkdir -p "/home/$KIRA_USER/.cargo"
 mkdir -p "/home/$KIRA_USER/Desktop"
 mkdir -p $SOURCES_LIST
 
-SETUP_CHECK="$KIRA_SETUP/kira-env-v0.0.50"
+SETUP_CHECK="$KIRA_SETUP/kira-env-v0.0.51"
 if [ ! -f "$SETUP_CHECK" ]; then
     echo "INFO: Setting up kira environment variables"
     touch $CARGO_ENV
