9770ac1cadf9b5b01a2940378ac743ecc4ef4484
James Truong <trueeyedev@gmail.com>
2020-12-14 00:27:11 +1100

    [KIP_51] use dns name instead of ip

--- local.tmp/kms-test/container/start.sh
+++ local.tmp/kms-test/container/start.sh
@@ -9,6 +9,4 @@ tmkms softsign keygen /root/.tmkms/secret_connection.key
 cd /root/.tmkms/ && ls
 tmkms softsign import ${SELF_KMS_RELEASE}/priv_validator_key.json /root/.tmkms/signing.key
 
-# ping 10.2.0.2
-
 sleep 10 && tmkms start -v -c ${SELF_KMS_RELEASE}/tmkms.toml
