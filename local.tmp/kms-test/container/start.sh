e50e34f729c8778de9ed0ff18e49791118ae2c15
James Truong <trueeyedev@gmail.com>
2020-12-14 05:28:55 +1100

    [KIP_51] tmkms init, flutter build --release option

--- local.tmp/kms-test/container/start.sh
+++ local.tmp/kms-test/container/start.sh
@@ -4,9 +4,11 @@ exec 2>&1
 set -e
 set -x
 
-tmkms softsign keygen /root/.tmkms/secret_connection.key
+tmkms init /root/.tmkms
+
+# tmkms softsign keygen /root/.tmkms/secret_connection.key
 
 cd /root/.tmkms/ && ls
 tmkms softsign import ${SELF_KMS_RELEASE}/priv_validator_key.json /root/.tmkms/signing.key
 
-sleep 10 && tmkms start -v -c ${SELF_KMS_RELEASE}/tmkms.toml
+sleep 10 && tmkms start -c ${SELF_KMS_RELEASE}/tmkms.toml
