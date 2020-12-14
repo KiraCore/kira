0cffca55a5d5c223cc0da85aa1aa13d947b98611
James Truong <trueeyedev@gmail.com>
2020-12-15 07:05:34 +1100

    [KIP_51] kms config file to /root/.tmkms

--- local.tmp/kms-test/container/start.sh
+++ local.tmp/kms-test/container/start.sh
@@ -7,8 +7,9 @@ set -x
 tmkms init /root/.tmkms
 
 # tmkms softsign keygen /root/.tmkms/secret_connection.key
+mv /root/tmkms.toml /root/.tmkms/
 
 cd /root/.tmkms/ && ls
-tmkms softsign import ${SELF_KMS_RELEASE}/priv_validator_key.json /root/.tmkms/signing.key
+tmkms softsign import /root/priv_validator_key.json /root/.tmkms/signing.key
 
-sleep 10 && tmkms start -v -c ${SELF_KMS_RELEASE}/tmkms.toml
+sleep 10 && tmkms start -v -c /root/.tmkms/tmkms.toml
