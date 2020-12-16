7a65f7b6779161161b85a6ebcc4d39710c9e5583
asmodat <asmodat@gmail.com>
2020-12-16 22:05:27 +0000

    intall priv key creation tools

--- workstation/setup/golang.sh
+++ workstation/setup/golang.sh
@@ -3,8 +3,11 @@
 set +e && source "/etc/profile" &>/dev/null && set -e
 exec &> >(tee -a "$KIRA_DUMP/setup.log")
 
-SETUP_CHECK="$KIRA_SETUP/go-v${GO_VERSION}" 
+SETUP_CHECK="$KIRA_SETUP/go-v${GO_VERSION}-0" 
 if [ ! -f "$SETUP_CHECK" ] ; then
+    echo "INFO: Ensuring golang is removed ..."
+    apt-get remove golang-go -y
+    apt-get remove --auto-remove golang-go -y
     echo "INFO: Installing latest go version $GO_VERSION https://golang.org/doc/install ..."
     wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
     tar -C /usr/local -xvf go$GO_VERSION.linux-amd64.tar.gz
--- workstation/setup/tools.sh
+++ workstation/setup/tools.sh
@@ -2,7 +2,7 @@
 set +e && source "/etc/profile" &>/dev/null && set -e
 exec &> >(tee -a "$KIRA_DUMP/setup.log")
 
-KIRA_SETUP_BASE_TOOLS="$KIRA_SETUP/base-tools-v0.1.8"
+KIRA_SETUP_BASE_TOOLS="$KIRA_SETUP/base-tools-v0.1.9"
 if [ ! -f "$KIRA_SETUP_BASE_TOOLS" ]; then
   echo "INFO: Update and Intall basic tools and dependencies..."
   apt-get update -y --fix-missing
@@ -51,9 +51,10 @@ if [ ! -f "$KIRA_SETUP_BASE_TOOLS" ]; then
   cd /home/$SUDO_USER
   TOOLS_DIR="/home/$SUDO_USER/tools"
   PRIV_KEYGEN_DIR="$TOOLS_DIR/priv-validator-key-gen"
+  KMS_KEYIMPORT_DIR="$TOOLS_DIR/tmkms-key-import"
   $KIRA_SCRIPTS/git-pull.sh "https://github.com/KiraCore/tools.git" "main" "$TOOLS_DIR" 555
   FILE_HASH=$(CDHelper hash SHA256 -p="$TOOLS_DIR" -x=true -r=true --silent=true -i="$TOOLS_DIR/.git,$TOOLS_DIR/.gitignore")
-  EXPECTED_HASH="37fc48248aead8af6d6e03a1497657431bae310b5cd71d6e6f74f7d4e3cd3de7"
+  EXPECTED_HASH="44e8638081186eab4edafe6524c34bbe5b1900c5350096f0ec4810de43542841"
 
   if [ "$FILE_HASH" != "$EXPECTED_HASH" ]; then
     echo "DANGER: Failed to check integrity hash of the kira tools !!!"
@@ -63,8 +64,16 @@ if [ ! -f "$KIRA_SETUP_BASE_TOOLS" ]; then
   fi
 
   # TODO: intall tools
-  #cd $PRIV_KEYGEN_DIR
-  #make install
+  cd $PRIV_KEYGEN_DIR
+  go build
+  make install
+
+  ls -l /bin/priv-validator-key-gen || echo "priv-validator-key-gen symlink not found"
+  rm /bin/priv-validator-key-gen || echo "Removing old priv-validator-key-gen symlink"
+  ln -s $PRIV_KEYGEN_DIR/priv-validator-key-gen /bin/priv-validator-key-gen || echo "priv-validator-key-gen symlink already exists"
+
+  # MNEMONIC=$(hd-wallet-derive --gen-words=24 --gen-key --format=jsonpretty -g | jq '.[0].mnemonic')
+  # priv-validator-key-gen --mnemonic="$MNEMONIC" # will create priv_validator_key.json in the current directory
 
   cd /home/$SUDO_USER
   touch $KIRA_SETUP_BASE_TOOLS
