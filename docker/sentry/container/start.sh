b9dbd13a3e30667f6da565b427b451a7e173b938
James Truong <trueeyedev@gmail.com>
2020-12-19 05:54:34 +1100

    [KIP_51] sentry_node_key from common dir

--- docker/sentry/container/start.sh
+++ docker/sentry/container/start.sh
@@ -10,7 +10,7 @@ rm -rf $SEKAID_HOME
 
 sekaid init --chain-id=testing testing --home=$SEKAID_HOME
 cp $COMMON_DIR/genesis.json $SEKAID_HOME/config/genesis.json
-cp $COMMON_DIR/node_key.json $SEKAID_HOME/config/node_key.json
+cp $COMMON_DIR/sentry_node_key.json $SEKAID_HOME/config/node_key.json
 cp $SELF_CONFIGS/config.toml $SEKAID_HOME/config/config.toml
 
 sekaid start --home=$SEKAID_HOME --rpc.laddr="tcp://0.0.0.0:26657"
