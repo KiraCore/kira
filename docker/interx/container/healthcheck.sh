928c317974d511f74b80beef085e37a7f7fe82fe
James Truong <trueeyedev@gmail.com>
2020-12-21 03:18:19 +1100

    [KIP_51] halt check for sentry, interx, frontend

--- docker/interx/container/healthcheck.sh
+++ docker/interx/container/healthcheck.sh
@@ -4,6 +4,11 @@ exec 2>&1
 set -x
 
 set +e && source "/etc/profile" &>/dev/null && set -e
+HALT_CHECK="${COMMON_DIR}/halt"
+
+if [ -f "$HALT_CHECK" ]; then
+  exit 0
+fi
 
 echo "INFO: Healthcheck => START"
 sleep 30 # rate limit
