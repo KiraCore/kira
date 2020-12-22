37d7ca831b3a7d9c784053f75ea9dcb1eec9ecab
James Truong <trueeyedev@gmail.com>
2020-12-23 02:28:57 +1100

    [KIP_51] update await interx

--- docker/interx/container/start.sh
+++ docker/interx/container/start.sh
@@ -14,9 +14,9 @@ while [ -f "$HALT_CHECK" ]; do
   sleep 30
 done
 
-while ! ping -c1 sentry &>/dev/null ; do
-    echo "INFO: Waiting for ping response form sentry node... (`date`)"
-    sleep 5
+while ! ping -c1 sentry &>/dev/null; do
+  echo "INFO: Waiting for ping response form sentry node... ($(date))"
+  sleep 5
 done
 
 if [ ! -f "$EXECUTED_CHECK" ]; then
@@ -25,7 +25,7 @@ if [ ! -f "$EXECUTED_CHECK" ]; then
   rm -f $SEKAI/INTERX/config.json
   mv $COMMON_DIR/config.json $SEKAI/INTERX
 
-  touch $EXECUTED_CHECK
 fi
 
 GRPC=dns:///$KIRA_SENTRY_IP:9090 RPC=http://$KIRA_SENTRY_IP:26657 make start
+touch $EXECUTED_CHECK
