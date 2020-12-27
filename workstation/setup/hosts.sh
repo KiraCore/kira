85a587f7f52b410be344f7766083aaf2cfddca77
asmodat <asmodat@gmail.com>
2020-12-27 23:07:35 +0000

    hosts change

--- workstation/setup/hosts.sh
+++ workstation/setup/hosts.sh
@@ -3,7 +3,7 @@
 set +e && source "/etc/profile" &>/dev/null && set -e
 # exec >> "$KIRA_DUMP/setup.log" 2>&1 && tail "$KIRA_DUMP/setup.log"
 
-SETUP_CHECK="$KIRA_SETUP/hosts-v0.0.4-$KIRA_REGISTRY_NAME" 
+SETUP_CHECK="$KIRA_SETUP/hosts-v0.0.5-$KIRA_REGISTRY_NAME" 
 if [ ! -f "$SETUP_CHECK" ] ; then
     echo "INFO: Setting up default hosts..."
     CDHelper text lineswap --insert="$KIRA_REGISTRY_IP $KIRA_REGISTRY_NAME" --prefix="$KIRA_REGISTRY_IP" --path=$HOSTS_PATH --prepend-if-found-not=True --silent=$SILENT_MODE
