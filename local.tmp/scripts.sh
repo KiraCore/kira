fa45cf73c3d67aa649d2b76e1546821589130c70
asmodat <asmodat@gmail.com>
2020-12-28 00:35:56 +0000

    subnet switch

--- local.tmp/scripts.sh
+++ local.tmp/scripts.sh
@@ -35,10 +35,10 @@ docker network rm kiranet || echo "Failed to remove kira network"
 docker network rm kmsnet || echo "Failed to remove kira network"
 
 docker network create --driver=bridge --subnet=10.2.0.0/16 kiranet
-docker network create --driver=bridge --subnet=10.1.0.0/16 kmsnet
+docker network create --driver=bridge --subnet=10.5.0.0/16 kmsnet
 
 docker run -d -P --restart=always --name validator --net=kiranet --ip 10.2.0.2 validator-test-0
-docker run -d -P --restart=always --name kms --net=kmsnet --ip 10.1.0.2 kms-test
+docker run -d -P --restart=always --name kms --net=kmsnet --ip 10.5.0.2 kms-test
 
 docker network connect kiranet kms
 docker network connect kmsnet validator
--- workstation/setup/hosts.sh
+++ workstation/setup/hosts.sh
@@ -3,7 +3,7 @@
 set +e && source "/etc/profile" &>/dev/null && set -e
 # exec >> "$KIRA_DUMP/setup.log" 2>&1 && tail "$KIRA_DUMP/setup.log"
 
-SETUP_CHECK="$KIRA_SETUP/hosts-v0.0.6-$KIRA_REGISTRY_NAME-$KIRA_REGISTRY_PORT-$KIRA_REGISTRY_IP" 
+SETUP_CHECK="$KIRA_SETUP/hosts-v0.0.7-$KIRA_REGISTRY_NAME-$KIRA_REGISTRY_PORT-$KIRA_REGISTRY_IP" 
 if [ ! -f "$SETUP_CHECK" ] ; then
     echo "INFO: Setting up default hosts..."
     CDHelper text lineswap --insert="$KIRA_REGISTRY_IP $KIRA_REGISTRY_NAME" --regex="$KIRA_REGISTRY_NAME" --path=$HOSTS_PATH --prepend-if-found-not=True
