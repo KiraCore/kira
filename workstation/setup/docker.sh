4cb3a03d725a41b6d96e421a38946cf7802cdc37
asmodat <asmodat@gmail.com>
2021-01-01 23:21:07 +0000

    ensure ll components are removed even if one of them failes

--- workstation/setup/docker.sh
+++ workstation/setup/docker.sh
@@ -10,7 +10,10 @@ SETUP_CHECK="$KIRA_SETUP/docker-v0.0.3"
 if [ ! -f "$SETUP_CHECK" ] || [ "${VERSION,,}" == "error" ] || [ "${ACTIVE,,}" != "active" ] ; then
     echo "INFO: Attempting to remove old docker..."
     service docker stop || echo "WARNING: Failed to stop docker servce"
-    apt remove --purge docker.io docker-ce docker-ce-cli containerd.io -y || echo "WARNING: Failed to remove docker"
+    apt remove --purge docker.io -y || echo "WARNING: Failed to remove docker.io"
+    apt remove --purge docker-ce -y || echo "WARNING: Failed to remove docker-ce"
+    apt remove --purge docker-ce-cli -y || echo "WARNING: Failed to remove docker-ce-cli"
+    apt remove --purge containerd.io -y || echo "WARNING: Failed to remove containerd.io"
     rm -rfv /etc/docker
     rm -rfv /var/lib/docker
     echo "INFO: Installing Docker..."
