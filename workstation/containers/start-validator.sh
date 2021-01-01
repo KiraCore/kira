11a8ab996a49f735ed886318ff4c0ec68c02a5b0
asmodat <asmodat@gmail.com>
2021-01-01 18:49:27 +0000

    the sentry has validator node as private peer but also connects "to" the validator. In some strange cases there were problems and the peer got gossiped where sentry has a persistent peer. To avoid gossip it was recommended to let de validator connect to the sentry and the sentry will keep it private. Also use the unconditional_peer_ids so no matter how many connection a sentry has, the validator will always be able to connect.

--- workstation/containers/start-validator.sh
+++ workstation/containers/start-validator.sh
@@ -29,6 +29,7 @@ echo "INFO: Setting up validator config files..."
 CDHelper text lineswap --insert="pex = false" --prefix="pex =" --path=$DOCKER_COMMON/validator
 CDHelper text lineswap --insert="persistent_peers = \"$SENTRY_SEED\"" --prefix="persistent_peers =" --path=$DOCKER_COMMON/validator
 CDHelper text lineswap --insert="addr_book_strict = false" --prefix="addr_book_strict =" --path=$DOCKER_COMMON/validator
+CDHelper text lineswap --insert="version = \"v2\"" --prefix="version =" --path=$DOCKER_COMMON/validator # fastsync
 
 GENESIS_SOURCE="/root/.simapp/config/genesis.json"
 GENESIS_DESTINATION="$DOCKER_COMMON/sentry/genesis.json"
