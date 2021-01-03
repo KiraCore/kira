269d3c818ee2d5495135f3a3c7de70d071b57319
asmodat <asmodat@gmail.com>
2021-01-03 03:58:06 +0000

    final networking issue resolve

--- workstation/networking.sh
+++ workstation/networking.sh
@@ -14,156 +14,63 @@ set -x
 
 echo "INFO: Ensuring UFW rules persistence"
 
-setup-after-rules() {
-    IFace=$(netstat -rn | grep -m 1 UG | awk '{print $8}' | xargs)
-    UWF_AFTER="/etc/ufw/after.rules"
-    #UWF_BEFORE="/etc/ufw/before.init"
-
-    cat >> $UWF_AFTER <<EOL
-#-DOCKER-BEHIND-UFW-V1-START
-*filter
-:DOCKER-USER - [0:0]
-:ufw-user-input - [0:0]
-
--A DOCKER-USER -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A DOCKER-USER -m conntrack --ctstate INVALID -j DROP
--A DOCKER-USER -i $IFace -j ufw-user-input
--A DOCKER-USER -i $IFace -j DROP
-
-COMMIT
-
-*nat
-:POSTROUTING ACCEPT [0:0]
--A POSTROUTING ! -o docker0 -s 172.17.0.0/16 -j MASQUERADE
--A POSTROUTING ! -o docker0 -s $KIRA_REGISTRY_SUBNET -j MASQUERADE
--A POSTROUTING ! -o docker0 -s $KIRA_VALIDATOR_SUBNET -j MASQUERADE
--A POSTROUTING ! -o docker0 -s $KIRA_SENTRY_SUBNET -j MASQUERADE
--A POSTROUTING ! -o docker0 -s $KIRA_SERVICE_SUBNET -j MASQUERADE
-
-COMMIT
-
-#-DOCKER-BEHIND-UFW-V1-END
-EOL
-
-#    cat > $UWF_BEFORE <<EOL
-##!/bin/sh
-#set -e
-#
-#case "\$1" in
-#start)
-#    # typically required
-#    ;;
-#stop)
-#    iptables -F DOCKER-USER || true
-#    iptables -A DOCKER-USER -j RETURN || true
-#    iptables -X ufw-user-input || true
-#    # typically required
-#    ;;
-#status)
-#    # optional
-#    ;;
-#flush-all)
-#    # optional
-#    ;;
-#*)
-#    echo "'\$1' not supported"
-#    echo "Usage: before.init {start|stop|flush-all|status}"
-#    ;;
-#esac
-#EOL
-
-chmod +x $UWF_AFTER
-#chmod +x $UWF_BEFORE
-}
-
 if [ "${INFRA_MODE,,}" == "local" ] ; then
     echo "INFO: Setting up demo mode networking..."
     # Removing DOCKER-USER CHAIN (it won't exist at first)
-firewall-cmd --permanent --direct --remove-chain ipv4 filter DOCKER-USER
+    firewall-cmd --permanent --direct --remove-chain ipv4 filter DOCKER-USER
+    
+    # Flush rules from DOCKER-USER chain (again, these won't exist at first; firewalld seems to remember these even if the chain is gone)
+    firewall-cmd --permanent --direct --remove-rules ipv4 filter DOCKER-USER
+    
+    # Add the DOCKER-USER chain to firewalld
+    firewall-cmd --permanent --direct --add-chain ipv4 filter DOCKER-USER
+    
+    # Add rules (see comments for details)
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "This allows docker containers to connect to the outside world"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s 172.18.0.0/16 -m comment --comment "allow internal docker communication"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s 172.17.0.0/16 -m comment --comment "allow internal docker communication"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_REGISTRY_SUBNET -m comment --comment "allow internal docker communication"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_VALIDATOR_SUBNET -m comment --comment "allow internal docker communication"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_SENTRY_SUBNET -m comment --comment "allow internal docker communication"
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_SERVICE_SUBNET -m comment --comment "allow internal docker communication"
+    
+    #Add as many ip or other rules and then run this command to block all other traffic
+    firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j REJECT -m comment --comment "reject all other traffic"
+    
+    echo "INFO: Default firewall zone: $(firewall-cmd --get-default-zone 2> /dev/null || echo "???")"
+    
+    IFace=$(netstat -rn | grep -m 1 UG | awk '{print $8}' | xargs)
+    firewall-cmd --permanent --new-zone=demo || echo "INFO: Zone local-mode already exists"
+    firewall-cmd --permanent --zone=demo --change-interface=$IFace
+    #firewall-cmd --permanent --zone=demo --remove-port=$KIRA_FRONTEND_PORT/tcp
+    firewall-cmd --permanent --zone=demo --add-port=$KIRA_INTERX_PORT/tcp
+    firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_P2P_PORT/tcp
+    firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_RPC_PORT/tcp
+    firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_GRPC_PORT/tcp
+    firewall-cmd --permanent --zone=demo --add-port=22/tcp
+    firewall-cmd --permanent --zone=demo --set-target=REJECT
+    firewall-cmd --runtime-to-permanent
+    
+    firewall-cmd --reload
+    firewall-cmd --get-zones
+    firewall-cmd --zone=demo --list-all 
+    firewall-cmd --set-default-zone=demo
+    firewall-cmd --complete-reload
+    firewall-cmd --check-config || echo "INFO: Failed to check firewall config"
 
-# Flush rules from DOCKER-USER chain (again, these won't exist at first; firewalld seems to remember these even if the chain is gone)
-firewall-cmd --permanent --direct --remove-rules ipv4 filter DOCKER-USER
+    echo "INFO: Default firewall zone: $(firewall-cmd --get-default-zone 2> /dev/null || echo "???")"
 
-# Add the DOCKER-USER chain to firewalld
-firewall-cmd --permanent --direct --add-chain ipv4 filter DOCKER-USER
+    # restart the services
+    systemctl daemon-reload
+    systemctl stop docker
+    systemctl stop firewalld
+    systemctl start firewalld
+    systemctl start docker
 
-# Add rules (see comments for details)
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "This allows docker containers to connect to the outside world"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s 172.18.0.0/16 -m comment --comment "allow internal docker communication"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s 172.17.0.0/16 -m comment --comment "allow internal docker communication"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_REGISTRY_SUBNET -m comment --comment "allow internal docker communication"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_VALIDATOR_SUBNET -m comment --comment "allow internal docker communication"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_SENTRY_SUBNET -m comment --comment "allow internal docker communication"
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j RETURN -s $KIRA_SERVICE_SUBNET -m comment --comment "allow internal docker communication"
-#firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -p tcp -m multiport --dports https -s 123.456.7.89/32 -j ACCEPT -m comment --comment "my allowed ip address to http and https ports"
-
-#Add as many ip or other rules and then run this command to block all other traffic
-firewall-cmd --permanent --direct --add-rule ipv4 filter DOCKER-USER 0 -j REJECT -m comment --comment "reject all other traffic"
-
-echo "INFO: Default firewall zone: $(firewall-cmd --get-default-zone 2> /dev/null || echo "???")"
-
-IFace=$(netstat -rn | grep -m 1 UG | awk '{print $8}' | xargs)
-firewall-cmd --permanent --new-zone=demo || echo "INFO: Zone local-mode already exists"
-firewall-cmd --permanent --zone=demo --change-interface=$IFace
-#firewall-cmd --permanent --zone=demo --remove-port=$KIRA_FRONTEND_PORT/tcp
-firewall-cmd --permanent --zone=demo --add-port=$KIRA_INTERX_PORT/tcp
-firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_P2P_PORT/tcp
-firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_RPC_PORT/tcp
-firewall-cmd --permanent --zone=demo --add-port=$KIRA_SENTRY_GRPC_PORT/tcp
-firewall-cmd --permanent --zone=demo --add-port=22/tcp
-firewall-cmd --permanent --zone=demo --set-target=DROP
-firewall-cmd --runtime-to-permanent
-
-# firewall-cmd --zone=demo --permanent --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" port port="22" protocol="tcp" accept'
-# firewall-cmd --zone=demo --permanent --add-rich-rule='rule family="ipv4" source address="0.0.0.0/0" reject'
-
-firewall-cmd --reload
-firewall-cmd --get-zones
-firewall-cmd --zone=demo --list-all 
-firewall-cmd --set-default-zone=demo
-
-# firewall-cmd --complete-reload # hard reset
-
-
-echo "INFO: Default firewall zone: $(firewall-cmd --get-default-zone 2> /dev/null || echo "???")"
-
-# restart the services
-systemctl daemon-reload
-systemctl stop docker
-systemctl stop firewalld
-systemctl start firewalld
-systemctl start docker
-
-    #ufw disable
-    #ufw --force reset
-    #ufw logging on # required to setup logging rules
-    #setup-after-rules
-    #ufw default allow outgoing
-    #ufw default deny incoming
-    #ufw allow 22 # SSH
-    #ufw allow $KIRA_FRONTEND_PORT 
-    #ufw allow $KIRA_INTERX_PORT
-    #ufw allow $KIRA_SENTRY_P2P_PORT
-    #ufw allow $KIRA_SENTRY_RPC_PORT
-    #ufw allow $KIRA_SENTRY_GRPC_PORT
-    #ufw status verbose
-    #echo "y" | ufw enable || :
-    #ufw status verbose
-    #ufw reload
-    #systemctl daemon-reload
-    #systemctl restart ufw
-    #ufw status verbose
-
-    # firewall-cmd --zone=public --add-masquerade --permanent
-    # firewall-cmd --reload
 
     echo "INFO: Restarting docker..."
-    #systemctl restart docker || ( journalctl -u docker | tail -n 20 && systemctl restart docker )
     systemctl restart NetworkManager docker || echo "WARNING: Failed to restart network manager"
 
-    # WARNING, following command migt disable SSH access
-    # CDHelper text lineswap --insert="ENABLED=yes" --prefix="ENABLED=" --path=/etc/ufw/ufw.conf --append-if-found-not=True
-    
 elif [ "${INFRA_MODE,,}" == "sentry" ] ; then
     echo "INFO: Setting up sentry mode networking..."
 
--- workstation/setup/network.sh
+++ workstation/setup/network.sh
@@ -2,7 +2,7 @@
 set +e && source "/etc/profile" &>/dev/null && set -e
 # exec >> "$KIRA_DUMP/setup.log" 2>&1 && tail "$KIRA_DUMP/setup.log"
 
-SETUP_CHECK="$KIRA_SETUP/network-v0.0.7" 
+SETUP_CHECK="$KIRA_SETUP/network-v0.0.8" 
 if [ ! -f "$SETUP_CHECK" ] ; then
     echo "INFO: Setting up networking dependencies..."
     apt-get update -y
@@ -14,21 +14,17 @@ if [ ! -f "$SETUP_CHECK" ] ; then
     echo "INFO: Installing generic dependencies..."
     apt-get install -y ufw firewalld
 
-    # We are using ufw so if someone enabled firewalld - disable it
-    # systemctl disable firewalld || echo "INFO: Failed to disable firewalld"
-    # systemctl stop firewalld || echo "INFO: Failed to stop firewalld"
+    # resolve firewalld service restart conflicts
+    systemctl disable ebtables || echo "INFO: Failed to disable ebtables"
+    systemctl mask ebtables
+    ln -s /sbin/iptables /usr/sbin/ || echo "INFO: Failed symlink creation"
+    ln -s /sbin/iptables-restore /usr/sbin/ || echo "INFO: Failed symlink creation"
+    ln -s /sbin/ip6tables /usr/sbin/ || echo "INFO: Failed symlink creation"
+    ln -s /sbin/ip6tables-restore /usr/sbin/ || echo "INFO: Failed symlink creation"
 
     systemctl enable firewalld || echo "INFO: Failed to disable firewalld"
     systemctl restart firewalld || echo "INFO: Failed to stop firewalld"
 
-    # allow loggs to be dropped
-    # touch /var/log/ufw.log && chown syslog:syslog /var/log/ufw.log
-
-    #DEFAULT_UFW="/etc/default/ufw"
-    #CDHelper text lineswap --insert="IPV6=no" --prefix="IPV6=" --path="$DEFAULT_UFW" --append-if-found-not=True
-    #CDHelper text lineswap --insert="DEFAULT_FORWARD_POLICY=\"DROP\"" --prefix="DEFAULT_FORWARD_POLICY=" --path="$DEFAULT_UFW" --append-if-found-not=True
-    #CDHelper text lineswap --insert="MANAGE_BUILTINS=yes" --prefix="MANAGE_BUILTINS=" --path="$DEFAULT_UFW" --append-if-found-not=True
-    ##CDHelper text lineswap --insert="DOCKER_OPTS=\"--iptables=false\"" --prefix="DOCKER_OPTS=" --path="$DEFAULT_UFW" --append-if-found-not=True
 
     touch $SETUP_CHECK
 else
