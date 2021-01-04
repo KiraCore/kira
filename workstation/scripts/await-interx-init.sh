d95824216f268cbd489c78a1524d8686ece0ec01
James Truong <trueeyedev@gmail.com>
2021-01-04 21:36:48 +1100

    [KIP_51] await interx to faucet

--- workstation/scripts/await-interx-init.sh
+++ workstation/scripts/await-interx-init.sh
@@ -26,6 +26,17 @@ while [ $i -le 20 ]; do
         echo "INFO: Success, interx was initalized"
     fi
 
+    echo "INFO: Awaiting interx build to test..."
+    FAUCET_ADDR=$(docker exec -i "interx" curl http://127.0.0.1:11000/api/faucet 2>/dev/null | xargs || echo "")
+
+    if [[ "${FAUCET_ADDR}" -eq "" ]]; then
+        sleep 30
+        echo "WARNING: INTERX is not built yet"
+        continue
+    else
+        echo "INFO: Success, interx was built"
+    fi
+
     echo "INFO: Awaiting interx build to finalize..."
     INTERX_STATUS_CODE=$(docker exec -i "interx" curl -s -o /dev/null -w '%{http_code}' http://127.0.0.1:11000/api/status 2>/dev/null | xargs || echo "")
 
