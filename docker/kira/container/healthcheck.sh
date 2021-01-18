05bbbca8980ee48e825c86ad1455a3ec58669b4c
asmodat <asmodat@gmail.com>
2021-01-18 14:43:52 +0000

    bugfix healthcheck

--- docker/kira/container/healthcheck.sh
+++ docker/kira/container/healthcheck.sh
@@ -3,27 +3,30 @@
 exec 2>&1
 set -x
 
+echo "INFO: Health check => START"
+sleep 30 # rate limit not to overextend the log files
+
 set +e && source "/etc/profile" &>/dev/null && set -e
 HALT_CHECK="${COMMON_DIR}/halt"
 
 if [ -f "$HALT_CHECK" ]; then
+  echo "INFO: Healtc heck => STOP (halted)"
   exit 0
 fi
-
-echo "INFO: Healthcheck => START"
-sleep 30 # rate limit
 
 find "/var/log/journal" -type f -size +256k -exec truncate --size=128k {} + || echo "INFO: Failed to truncate journal"
 find "$SELF_LOGS" -type f -size +256k -exec truncate --size=128k {} + || echo "INFO: Failed to truncate self logs"
 
 
-if [ "${HALT_CHECK,,}" == "sentry" ] || [ "${HALT_CHECK,,}" == "priv_sentry" ] ; then
-    source $SELF_CONTAINER/sentry/healthcheck.sh
-elif [ "${HALT_CHECK,,}" == "snapshoot" ] ; then
-    source $SELF_CONTAINER/snapshoot/healthcheck.sh
-elif [ "${HALT_CHECK,,}" == "validator" ] ; then
-    source $SELF_CONTAINER/validator/healthcheck.sh
+if [ "${NODE_TYPE,,}" == "sentry" ] || [ "${NODE_TYPE,,}" == "priv_sentry" ] ; then
+    $SELF_CONTAINER/sentry/healthcheck.sh
+elif [ "${NODE_TYPE,,}" == "snapshoot" ] ; then
+    $SELF_CONTAINER/snapshoot/healthcheck.sh
+elif [ "${NODE_TYPE,,}" == "validator" ] ; then
+    $SELF_CONTAINER/validator/healthcheck.sh
 else
   echo "ERROR: Unknown node type '$NODE_TYPE'"
   exit 1
 fi
+
+exit 0
