452c05bf1d71c9e6728f94b74a96ff6797e0f6b1
asmodat <asmodat@gmail.com>
2021-01-22 21:31:26 +0000

    use fully formatted grpc

--- workstation/containers/start-priv-sentry.sh
+++ workstation/containers/start-priv-sentry.sh
@@ -67,7 +67,7 @@ docker run -d \
     -e NETWORK_NAME="$NETWORK_NAME" \
     -e CFG_moniker="KIRA ${CONTAINER_NAME^^} NODE" \
     -e CFG_pex="false" \
-    -e CFG_grpc_laddr="127.0.0.1:$DEFAULT_GRPC_PORT" \
+    -e CFG_grpc_laddr="tcp://127.0.0.1:$DEFAULT_GRPC_PORT" \
     -e CFG_rpc_laddr="tcp://127.0.0.1:$DEFAULT_RPC_PORT" \
     -e CFG_persistent_peers="tcp://$VALIDATOR_SEED" \
     -e CFG_private_peer_ids="$VALIDATOR_NODE_ID,$SNAPSHOOT_NODE_ID,$SENTRYT_NODE_ID,$PRIV_SENTRYT_NODE_ID" \
