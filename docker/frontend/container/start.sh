bff10f8a433937d032b18abc94e8dc25a789bd2f
asmodat <asmodat@gmail.com>
2021-01-23 01:53:03 +0000

    build fix frontend & nginx restart

--- docker/frontend/container/start.sh
+++ docker/frontend/container/start.sh
@@ -4,10 +4,13 @@ exec 2>&1
 set -e
 set -x
 
-echo "Staring frontend..."
+echo "INFO: Staring frontend v0.0.1"
 
 EXECUTED_CHECK="$COMMON_DIR/executed"
 HALT_CHECK="${COMMON_DIR}/halt"
+BUILD_SOURCE="/root/kira-frontend/src/build/web"
+BUILD_DESTINATION="/usr/share/nginx/html"
+FRONTEND_SRC="/root/kira-frontend/src"
 
 while [ -f "$HALT_CHECK" ]; do
   sleep 30
@@ -20,8 +23,28 @@ done
 echo "INFO: INTERX IP Found: $(getent hosts interx | awk '{ print $1 }')"
 
 if [ ! -f "$EXECUTED_CHECK" ]; then
-    sleep 1 # TODO: setup
+    echo "INFO: Building fronted from $REPO..."
+    cd /root
+    git clone ${REPO}
+    cd $FRONTEND_SRC
+    
+    git checkout ${BRANCH}
+    
+    cat > $FRONTEND_SRC/assets/config.json << EOL
+{
+  "api_url": "http://0.0.0.0:11000/api"
+}
+EOL
+
+    flutter pub get
+    flutter build web --release
+
+    mkdir -p "$BUILD_DESTINATION"
+    cp -v -f "$BUILD_SOURCE" "$BUILD_DESTINATION"
+
     touch $EXECUTED_CHECK
 fi
 
+service nginx restart
+sleep 1
 nginx -g 'daemon off;'
