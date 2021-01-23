a6ed8f817feaef4890b82996c780a4d140b874e8
asmodat <asmodat@gmail.com>
2021-01-23 03:20:13 +0000

    build frontend on start

--- docker/frontend/container/start.sh
+++ docker/frontend/container/start.sh
@@ -9,9 +9,8 @@ echo "INFO: Build hash -> ${BUILD_HASH} -> Branch: ${BRANCH} -> Repo: ${REPO}"
 
 EXECUTED_CHECK="$COMMON_DIR/executed"
 HALT_CHECK="${COMMON_DIR}/halt"
-BUILD_SOURCE="/root/kira-frontend/src/build/web"
+BUILD_SOURCE="${FRONTEND_SRC}/build/web"
 BUILD_DESTINATION="/usr/share/nginx/html"
-FRONTEND_SRC="/root/kira-frontend/src"
 
 while [ -f "$HALT_CHECK" ]; do
   sleep 30
@@ -25,20 +24,11 @@ echo "INFO: INTERX IP Found: $(getent hosts interx | awk '{ print $1 }')"
 
 if [ ! -f "$EXECUTED_CHECK" ]; then
     echo "INFO: Building fronted from $REPO..."
-    cd /root
-    git clone ${REPO}
-    cd $FRONTEND_SRC
-    git checkout ${BRANCH}
-    
     cat > $FRONTEND_SRC/assets/config.json << EOL
 {
   "api_url": "http://0.0.0.0:11000/api"
 }
 EOL
-
-    flutter pub get
-    flutter build web --release
-
     mkdir -p "$BUILD_DESTINATION"
     cp -v -f "$BUILD_SOURCE" "$BUILD_DESTINATION"
 
