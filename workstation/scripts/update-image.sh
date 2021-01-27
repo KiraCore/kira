92b6a7b53193efd1cc0de12b0d504174c991bddd
asmodat <asmodat@gmail.com>
2021-01-27 23:33:25 +0000

    echo beautify

--- workstation/scripts/update-image.sh
+++ workstation/scripts/update-image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-exec 2>&1
-
 set +e && source "/etc/profile" &>/dev/null && set -e
+source $KIRA_MANAGER/utils.sh
 set -x
 
 IMAGE_DIR=$1
@@ -33,18 +32,11 @@ touch $KIRA_SETUP_FILE
 
 cd $IMAGE_DIR
 
-# INTEGRITY_HASH="$(echo "
-# $(CDHelper hash SHA256 -p="$IMAGE_DIR" -x=true -r=true --silent=true)
-# $IMAGE_DIR-$IMAGE_NAME-$IMAGE_TAG
-# $ARG1_KEY-$ARG1_VAL
-# $ARG2_KEY-$ARG2_VAL
-# $ARG3_KEY-$ARG3_VAL
-# " | md5sum | awk '{print $1}')"
-
 # adding integrity to the hash enables user to update based on internal image state
 OLD_HASH=$(cat $KIRA_SETUP_FILE)
 NEW_HASH="$(CDHelper hash SHA256 -p="$IMAGE_DIR" -x=true -r=true --silent=true)-$INTEGRITY"
 
+set +x
 echo "------------------------------------------------"
 echo "|         STARTED: IMAGE UPDATE v0.0.1         |"
 echo "------------------------------------------------"
@@ -57,19 +49,20 @@ echo "|     BUILD ARG 1: $ARG1_KEY=$ARG1_VAL"
 echo "|     BUILD ARG 2: $ARG2_KEY=$ARG2_VAL"
 echo "|     BUILD ARG 3: $ARG3_KEY=$ARG3_VAL"
 echo "------------------------------------------------"
+set -x
 
 if [[ $($KIRAMGR_SCRIPTS/image-updated.sh "$IMAGE_DIR" "$IMAGE_NAME" "$IMAGE_TAG" "$INTEGRITY") != "True" ]]; then
 
     if [ "$OLD_HASH" != "$NEW_HASH" ]; then
-        echo "WARNING: Image '$IMAGE_DIR' hash changed from $OLD_HASH to $NEW_HASH"
+        echoWarn "WARNING: Image '$IMAGE_DIR' hash changed from $OLD_HASH to $NEW_HASH"
     else
-        echo "INFO: Image hash '$OLD_HASH' did NOT changed, but image was NOT found"
+        echoInfo "INFO: Image hash '$OLD_HASH' did NOT changed, but image was NOT found"
     fi
 
     # NOTE: This script automaitcaly removes KIRA_SETUP_FILE file (rm -fv $KIRA_SETUP_FILE)
     $KIRAMGR_SCRIPTS/delete-image.sh "$IMAGE_DIR" "$IMAGE_NAME" "$IMAGE_TAG" #1
 
-    echo "Creating new '$IMAGE_NAME' image..."
+    echoInfo "INFO: Creating new '$IMAGE_NAME' image..."
     docker build --network=host --tag="$IMAGE_NAME" --build-arg BUILD_HASH="$NEW_HASH" --build-arg "$ARG1_KEY=$ARG1_VAL" --build-arg "$ARG2_KEY=$ARG2_VAL" --build-arg "$ARG3_KEY=$ARG3_VAL" --file "$IMAGE_DIR/Dockerfile" .
 
     docker image ls # list docker images
@@ -78,13 +71,15 @@ if [[ $($KIRAMGR_SCRIPTS/image-updated.sh "$IMAGE_DIR" "$IMAGE_NAME" "$IMAGE_TAG
     docker push $KIRA_REGISTRY/$IMAGE_NAME
     echo $NEW_HASH >$KIRA_SETUP_FILE
 else
-    echo "INFO: Image '$IMAGE_DIR' ($NEW_HASH) did NOT change"
+    echoInfo "INFO: Image '$IMAGE_DIR' ($NEW_HASH) did NOT change"
 fi
 
 #wget -qO - localhost:5000/v2/_catalog
 curl "$KIRA_REGISTRY/v2/_catalog"
 curl "$KIRA_REGISTRY/v2/$IMAGE_NAME/tags/list"
 
+set +x
 echo "------------------------------------------------"
 echo "|        FINISHED: IMAGE UPDATE v0.0.1         |"
 echo "------------------------------------------------"
+set -x
