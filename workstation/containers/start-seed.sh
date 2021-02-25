71bcd4951f0d09bc08738fdd96e2449b099a040b
James Truong <trueeyedev@gmail.com>
2021-02-26 05:52:52 +1100

    seed healthcheck enabled. remove sentry from private peer id in seed

--- workstation/containers/start-seed.sh
+++ workstation/containers/start-seed.sh
@@ -61,7 +61,7 @@ if [ "${EXTERNAL_SYNC,,}" == "true" ]; then
     echoInfo "INFO: Synchronisation using external genesis file ($LOCAL_GENESIS_PATH) will be performed"
     cp -f -a -v "$KIRA_CONFIGS/genesis.json" "$COMMON_PATH/genesis.json"
     CFG_seeds=""
-    CFG_persistent_peers="tcp://$PRIV_SENTRY_SEED,tcp://$SENTRY_SEED"
+    CFG_persistent_peers="tcp://$SENTRY_SEED"
 else
     CFG_seeds=""
     CFG_persistent_peers=""
@@ -88,8 +88,8 @@ docker run -d \
     -e CFG_p2p_laddr="tcp://0.0.0.0:$DEFAULT_P2P_PORT" \
     -e CFG_seeds="$CFG_seeds" \
     -e CFG_persistent_peers="$CFG_persistent_peers" \
-    -e CFG_private_peer_ids="$SNAPSHOT_NODE_ID,$PRIV_SENTRY_NODE_ID,$SENTRY_NODE_ID" \
-    -e CFG_unconditional_peer_ids="$SNAPSHOT_NODE_ID,$PRIV_SENTRY_NODE_ID,$SENTRY_NODE_ID" \
+    -e CFG_private_peer_ids="$PRIV_SENTRY_NODE_ID" \
+    -e CFG_unconditional_peer_ids="$PRIV_SENTRY_NODE_ID,$SENTRY_NODE_ID" \
     -e CFG_addr_book_strict="false" \
     -e CFG_seed_mode="true" \
     -e CFG_max_num_outbound_peers="32" \
