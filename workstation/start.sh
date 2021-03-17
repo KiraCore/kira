bb08816bbf58823bd1d508e8ecb78005eea15917
asmodat <asmodat@gmail.com>
2021-03-17 15:15:33 +0000

    create snap on the sentry

--- workstation/start.sh
+++ workstation/start.sh
@@ -139,7 +139,7 @@ if [ "${INFRA_MODE,,}" == "local" ] ; then
     $KIRA_MANAGER/containers/start-interx.sh 
     $KIRA_MANAGER/containers/start-frontend.sh 
 elif [ "${INFRA_MODE,,}" == "sentry" ] ; then
-    $KIRA_MANAGER/containers/start-sentry.sh
+    $KIRA_MANAGER/containers/start-sentry.sh "true"
     $KIRA_MANAGER/containers/start-priv-sentry.sh 
     $KIRA_MANAGER/containers/start-seed.sh
     $KIRA_MANAGER/containers/start-interx.sh 
