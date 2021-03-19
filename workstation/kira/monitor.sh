cadcd7997fc2957d15891f6958b59a4f0ca54eee
James Truong <trueeyedev@gmail.com>
2021-03-20 04:24:22 +1100

    [backup-auto] get date

--- workstation/kira/monitor.sh
+++ workstation/kira/monitor.sh
@@ -103,7 +103,7 @@ echo "(1) Log Started..." >"${AUTO_BACKUP_SCAN_PATH}.log"
 
 if [ -f $SCAN_DONE ] && [[ $AUTO_BACKUP_ENABLED = "Enabled" ]]; then
     echo "(2) Enabled pass" >"${AUTO_BACKUP_SCAN_PATH}.log"
-    ELAPSED_TIME=""
+    ELAPSED_TIME=0
     if [ ! -z "$AUTO_BACKUP_EXECUTED_TIME" ]; then
         ELAPSED_TIME=$(($(date -u +%s) - $AUTO_BACKUP_EXECUTED_TIME))
     fi
@@ -111,7 +111,7 @@ if [ -f $SCAN_DONE ] && [[ $AUTO_BACKUP_ENABLED = "Enabled" ]]; then
     INTERVAL_AS_SECOND=$(($AUTO_BACKUP_INTERVAL * 60))
     echo "(4) Interval second: ${INTERVAL_AS_SECOND}" >"${AUTO_BACKUP_SCAN_PATH}.log"
     if [ -z "$AUTO_BACKUP_EXECUTED_TIME" ] || [ $ELAPSED_TIME -gt $INTERVAL_AS_SECOND ]; then
-        AUTO_BACKUP_EXECUTED_TIME=$(date -u %s)
+        AUTO_BACKUP_EXECUTED_TIME=$(date -u +%s)
         echo "(5) time to backup: ${AUTO_BACKUP_EXECUTED_TIME}" >"${AUTO_BACKUP_SCAN_PATH}.log"
         CDHelper text lineswap --insert="AUTO_BACKUP_EXECUTED_TIME=$AUTO_BACKUP_EXECUTED_TIME" --prefix="AUTO_BACKUP_EXECUTED_TIME=" --path=$ETC_PROFILE --append-if-found-not=True
 
