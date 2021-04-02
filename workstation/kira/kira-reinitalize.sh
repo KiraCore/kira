cf99b2e25783d4818f8ccc27384357e82ceda9b4
asmodat <asmodat@gmail.com>
2021-04-02 19:50:09 +0000

    fix reinitalization bug

--- workstation/kira/kira-reinitalize.sh
+++ workstation/kira/kira-reinitalize.sh
@@ -88,7 +88,20 @@ else
         mkdir -p $KIRA_SETUP
     fi
 
-    [ "$INFRA_BRANCH" != "$NEW_BRANCH" ] && CDHelper text lineswap --insert="INFRA_BRANCH=$NEW_BRANCH" --prefix="INFRA_BRANCH=" --path=$ETC_PROFILE --append-if-found-not=True
+    if [ "$INFRA_BRANCH" != "$NEW_BRANCH" ] ; then
+        # All branches should have the same name across all repos to be considered compatible
+        if [[ $NEW_BRANCH == mainnet* ]] || [[ $NEW_BRANCH == testnet* ]] ; then
+            DEFAULT_BRANCH="$NEW_BRANCH"
+            SEKAI_BRANCH="$DEFAULT_BRANCH"
+            FRONTEND_BRANCH="$DEFAULT_BRANCH"
+            INTERX_BRANCH="$DEFAULT_BRANCH"
+        fi
+
+        CDHelper text lineswap --insert="INFRA_BRANCH=$NEW_BRANCH" --prefix="INFRA_BRANCH=" --path=$ETC_PROFILE --append-if-found-not=True
+        CDHelper text lineswap --insert="SEKAI_BRANCH=$SEKAI_BRANCH" --prefix="SEKAI_BRANCH=" --path=$ETC_PROFILE --append-if-found-not=True
+        CDHelper text lineswap --insert="FRONTEND_BRANCH=$FRONTEND_BRANCH" --prefix="FRONTEND_BRANCH=" --path=$ETC_PROFILE --append-if-found-not=True
+        CDHelper text lineswap --insert="INTERX_BRANCH=$INTERX_BRANCH" --prefix="INTERX_BRANCH=" --path=$ETC_PROFILE --append-if-found-not=True
+    fi
     source $INIT_SCRIPT_OUT "$NEW_BRANCH"
 fi
 
