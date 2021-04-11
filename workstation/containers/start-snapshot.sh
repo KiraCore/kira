e7eb95f837e6cc72ef1ab9c1f71aa8f2d744004b
asmodat <asmodat@gmail.com>
2021-04-11 22:36:25 +0000

    fix zip

--- workstation/containers/start-snapshot.sh
+++ workstation/containers/start-snapshot.sh
@@ -84,7 +84,8 @@ rm -fv $SNAP_DESTINATION
 rm -rfv $SNAP_DESTINATION_DIR
 if [ -f "$KIRA_SNAP_PATH" ] ; then
     echoInfo "INFO: State snapshot was found, cloning..."
-    cp -a -v -f $KIRA_SNAP_PATH "$SNAP_DESTINATION"
+    #cp -a -v -f $KIRA_SNAP_PATH "$SNAP_DESTINATION"
+    zip -F $KIRA_SNAP_PATH --out $SNAP_DESTINATION
     mkdir -p "$SNAP_DESTINATION_DIR"
     unzip $SNAP_DESTINATION -d $SNAP_DESTINATION_DIR
     rm -fv $SNAP_DESTINATION
