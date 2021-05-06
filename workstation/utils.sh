b0168a3015e769332c128471e96ac605b9f0c8b9
asmodat <asmodat@gmail.com>
2021-05-06 16:37:27 +0000

    syntax

--- workstation/utils.sh
+++ workstation/utils.sh
@@ -365,7 +365,7 @@ function isSubStr {
 }
 
 function isCommand {
-    if command -v docker ; then echo "true" ; else echo "false" ; fi
+    if command -v "$1" ; then echo "true" ; else echo "false" ; fi
 }
 
 displayAlign() {
