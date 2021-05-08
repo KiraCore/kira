4c94e5206d6fb99c496be64fc0c22ef0b9b42ce3
asmodat <asmodat@gmail.com>
2021-05-08 02:12:03 +0000

    allow duplicates

--- workstation/containers/start-sentry.sh
+++ workstation/containers/start-sentry.sh
@@ -86,7 +86,7 @@ docker run -d \
     -e CFG_unconditional_peer_ids="$VALIDATOR_NODE_ID,$SNAPSHOT_NODE_ID,$PRIV_SENTRY_NODE_ID,$SEED_NODE_ID" \
     -e CFG_addr_book_strict="true" \
     -e CFG_seed_mode="false" \
-    -e CFG_allow_duplicate_ip="false" \
+    -e CFG_allow_duplicate_ip="true" \
     -e CFG_max_num_outbound_peers="64" \
     -e CFG_max_num_inbound_peers="128" \
     -e CFG_handshake_timeout="30s" \
