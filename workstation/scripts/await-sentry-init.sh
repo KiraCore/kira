9350066861e0bc5dee4db170bcd8075fc884f434
asmodat <asmodat@gmail.com>
2021-05-08 01:08:16 +0000

    do not wait longer than 10 minutes

--- workstation/scripts/await-sentry-init.sh
+++ workstation/scripts/await-sentry-init.sh
@@ -154,17 +154,17 @@ if [ "${SYNC_AWAIT,,}" == "true" ] ; then
         fi
 
         set -x
-        #SYNCING=$(echo $STATUS | jsonQuickParse "catching_up" 2>/dev/null || echo -n "")
-        #($(isNullOrEmpty "$SYNCING")) && SYNCING="false"
-        #HEIGHT=$(echo "$STATUS" | jsonQuickParse "latest_block_height" 2>/dev/null || echo -n "")
-        #(! $(isNaturalNumber "$HEIGHT")) && HEIGHT=0
         HEIGHT=$(globGet "${CONTAINER_NAME}_BLOCK")
         SYNCING=$(globGet "${CONTAINER_NAME}_SYNCING")
         LATEST_BLOCK=$(globGet LATEST_BLOCK)
         MIN_HEIGH=$(globGet MIN_HEIGHT)
         DELTA_TIME=$(timerSpan BLOCK_HEIGHT_SPAN)
 
-        timerStart BLOCK_HEIGHT_SPAN
+        BLOCKS_LEFT=$(($MIN_HEIGH - $HEIGHT))
+        DELTA_HEIGHT=$(($BLOCKS_LEFT_OLD - $BLOCKS_LEFT))
+        BLOCKS_LEFT_OLD=$BLOCKS_LEFT
+
+        [[ $DELTA_HEIGHT -gt 0 ]] && timerStart BLOCK_HEIGHT_SPAN
         
         [[ $LATEST_BLOCK -gt $MIN_HEIGH ]] && MIN_HEIGH=$LATEST_BLOCK
         
@@ -175,16 +175,12 @@ if [ "${SYNC_AWAIT,,}" == "true" ] ; then
 
         [[ $DELTA_TIME -gt 600 ]] && echoErr "ERROR: $CONTAINER_NAME failed to catch up new blocks for over 10 minutes!" && exit 1
 
-        BLOCKS_LEFT=$(($MIN_HEIGH - $HEIGHT))
-        DELTA_HEIGHT=$(($BLOCKS_LEFT_OLD - $BLOCKS_LEFT))
-        BLOCKS_LEFT_OLD=$BLOCKS_LEFT
-        
         set +x
         if [[ $BLOCKS_LEFT -gt 0 ]] && [[ $DELTA_HEIGHT -gt 0 ]] && [[ $DELTA_TIME -gt 0 ]] ; then
             TIME_LEFT=$((($BLOCKS_LEFT * $DELTA_TIME) / $DELTA_HEIGHT))
             echoInfo "INFO: Estimated time left until catching up with min.height: $(prettyTime $TIME_LEFT)"
         fi
-        echoInfo "INFO: Minimum height: $LATEST_BLOCK, current height: $HEIGHT, catching up: $SYNCING"
+        echoInfo "INFO: Minimum height: $LATEST_BLOCK, current height: $HEIGHT, catching up: $SYNCING ($DELTA_HEIGHT)"
         echoInfo "INFO: Do NOT close your terminal, waiting for '$CONTAINER_NAME' to finish catching up..."
         set -x
         sleep 30
