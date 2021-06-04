c7ad11eafb123076b261e0b81f330c8eb1ae95ca
asmodat <asmodat@gmail.com>
2021-06-04 04:29:45 +0000

    hotfix ln

--- docker/kira/container/snapshot/start.sh
+++ docker/kira/container/snapshot/start.sh
@@ -61,7 +61,7 @@ if [ ! -f "$EXECUTED_CHECK" ]; then
     set +e && source "/etc/profile" &>/dev/null && set -e
     cp -afv $COMMON_DIR/node_key.json $SEKAID_HOME/config/node_key.json
 
-    if (! $(isFileEmpty "$COMMON_DIR/addrbook.json")) ; then
+    if ($(isFileJson "$COMMON_DIR/addrbook.json")) ; then
         echoInfo "INFO: Importing external addrbook file..."
         cp -afv "$COMMON_DIR/addrbook.json" $SEKAID_HOME/config/addrbook.json
     fi
